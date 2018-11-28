26be1eaf36beb171c0c49ca25d76a2499d0142b5
Raúl De Zamacona <raul@pcuervo.com>
2018-11-27 18:03:13 -0600

    Deshabilitar scroll con mouse wheel encima del input number de entradas/salidas

--- app.js
+++ app.js
@@ -2187,8 +2187,18 @@ conAngular.run(['$rootScope', '$state', '$cookies', '$http', 'AuthenticationServ
         if (restrictedPage && ! $rootScope.loggedIn ) {
             event.preventDefault();
             $state.go('/login');
-        }
+        }
 
+    });
+
+    $(document).on('focus', 'input[type=number]', function (e) {
+        $(this).on('mousewheel.disableScroll', function (e) {
+            e.preventDefault();
+        })
+    });
+
+    $(document).on('blur', 'input[type=number]', function (e) {
+      $(this).off('mousewheel.disableScroll');
     });
 
 }]);
